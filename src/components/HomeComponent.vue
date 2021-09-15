77a864d85b92c33b2f8216b5f7850345538cf5ea
Maduka Nuwantha <mbnuwantha90@gmail.com>
2021-09-15 14:31:04 +0530

    Assignment done

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container-fluid">
+    <div class="container bg-white rounded p-5">
+      <h2 class="pb-4">Shape Finder</h2>
+      <div class="row">
+        <div class="col">
+          <img
+            src="../assets/Square.png"
+            alt=""
+            class="img-thumbnail"
+            v-on:click="(id = '01'), (name = 'Square')"
+          />
+        </div>
+        <div class="col">
+          <img
+            src="../assets/Circle.png"
+            alt=""
+            class="img-thumbnail"
+            v-on:click="(id = '02'), (name = 'Circle')"
+          />
+        </div>
+        <div class="col">
+          <img
+            src="../assets/Triangle.png"
+            alt=""
+            class="img-thumbnail"
+            v-on:click="(id = '03'), (name = 'Triangle')"
+          />
+        </div>
+      </div>
+      <div class="row p-5 text-start">
+        <div class="col">
+          <label for="shape-name" class="form-lable">Selected Shape Name</label>
+          <input
+            id="shape-name"
+            class="form-control"
+            type="text"
+            v-model="name"
+          />
+        </div>
+        <div class="col">
+          <label for="shape-id" class="form-lable">Selected Shape ID</label>
+          <input id="shape-id" class="form-control" type="text" v-model="id" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "home-component",
+  data() {
+    return {
+      id: null,
+      name: null,
+    };
+  },
+};
+</script>
+<style>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <HomeComponent />
+  </div>
+</template>
+<script>
+import HomeComponent from "../components/HomeComponent.vue";
+export default {
+  components: { HomeComponent },
+  name: "home",
+};
+</script>
+<style>
+</style>
